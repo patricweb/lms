6eea6af943847f2e29741a8972b7eae3111e373b
patricweb <matejtradir@gmail.com>
2025-11-10 14:58:55 +0200

    .

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EduLMS - Modern Learning Platform</title>
 
-    @vite(['resources/css/app.css', 'resources/js/search.js'])
+    @vite(['resources/css/app.css')
     @livewireStyles
 </head>
 <body class="font-sans bg-[#182023] text-[#f4f4f4] antialiased">
