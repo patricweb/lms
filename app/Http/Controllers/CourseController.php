f0ad02126fd8ae4b0b43322b5903bf7cd95622a5
patricweb <matejtradir@gmail.com>
2025-11-13 00:45:09 +0200

    hi

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -96,7 +96,7 @@ class CourseController extends Controller
         return view('courses.create', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function save(Request $request)
     {
         $user = Auth::user();
 
@@ -104,5 +104,97 @@ class CourseController extends Controller
         {
             return view('errors.403');
         }
+
+        $validated = $request->validate([
+            'title' => 'required|min:3|max:255',
+            'description' => 'required|min:10',
+            'price' => 'nullable|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'thumbnail' => 'nullable|image|mimes:jped,png,jpg|max:2048'
+        ]);
+
+        if ($request->hasFile('thumbnail'))
+        {
+            $validated['thumbnail'] = $request->file('thumbnail')->store('thumbnails', 'public');
+        }
+
+        $validated['teacher_id'] = $user->id;
+        $validated['is_published'] = false;
+
+        $course = Course::create($validated);
+
+        return redirect()->route('courses.show', $course)->with('success', 'Курс создан успешно!');
+    }
+
+    public function edit(Course $course)
+    {
+        $user = Auth::user();
+
+        if ($user->role !== 'teacher' || $user->role !== 'admin')
+        {
+            return view('errors.403');
+        }
+        if ($user->role === 'teacher' || $user->role === 'admin' && $user->id !== $course->teacher_id)
+        {
+            return view('errors.403');
+        }
+
+        $categories = Category::all();
+
+        return view('course.edit', compact('course', 'categories'));
+    }
+
+    public function update (Request $request, Course $course)
+    {
+        $user = Auth::user();
+
+        if ($user->role !== 'teacher' || $user->role !== 'admin')
+        {
+            return view('errors.403');
+        }
+        if ($user->role === 'teacher' || $user->role === 'admin' && $user->id !== $course->teacher_id)
+        {
+            return view('errors.403');
+        }
+
+        $validated = $request->validate([
+            'title' => 'nullable|min:3|max:255',
+            'description' => 'nullable|min:10',
+            'price' => 'nullable|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'is_published' => 'boolean'
+        ]);
+
+        if ($request->hasFile('thumbnail'))
+        {
+            if ($course->thumbnail)
+            {
+                Storage::disk('public')->delete($course->thumbnail);
+            }
+
+            $validated['thumbnail'] = $request->file('thumbnail')->store('thumbnail', 'public');
+        }
+
+        $course->update($validated);
+
+        return redirect()->route('courses.show', $course)->with('success', 'Курс успешно обновлен!');
+    }
+
+    public function destroy(Course $course)
+    {
+        $user = Auth::user();
+
+        if ($user->role !== 'teacher' || $user->role !== 'admin')
+        {
+            return view('errors.403');
+        }
+        if ($user->role === 'teacher' || $user->role === 'admin' && $user->id !== $course->teacher_id)
+        {
+            return view('errors.403');
+        }
+
+        $course->delete();
+
+        return redirect()->route('courses.index')->with('success', "Курс успешно удален");
     }
 }
